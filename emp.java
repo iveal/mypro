1ec2c70b9b556480b4e636aee27382cb479f6d8e
iveal <673358990@qq.com>
2015-11-05 10:13:38 +0800

    add emp.java&hello.java

--- emp.java
+++ emp.java
@@ -1,6 +1,9 @@
 ﻿import java.io.*;
+import java.util.*;
 public class emp implements Serializable{
 	private Integer empno;
 	private String ename;
 	private String job;
+	private String life;
+	private String comm;
 }
--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 ﻿public class Hello{
 public static void main(String args[]){
- Stystem.out.println("www.iveal.com");
+ Stystem.out.println("http://www.iveal.com");
  }
 }
